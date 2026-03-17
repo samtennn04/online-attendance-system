e6e40570687bc01b476be3288cb0a718e7c8dcd4
Samten Wangchuk <samten17396504@gmail.com>
2026-03-17 12:31:21 +0600

    Configured db.js and server.js for Render deployment

--- backend/db.js
+++ backend/db.js
@@ -8,3 +8,18 @@
 // })
 
 // module.exports = db.promise()
+
+// db.js
+const mysql = require("mysql2");
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,       // from Render environment variables
+  user: process.env.DB_USER,       // from Render environment variables
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = db.promise(); // allows async/await in server.js
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const path = require('path');
 const express = require("express");
+const db = require("./db");
 const mysql = require("mysql2");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -30,12 +31,12 @@ const JWT_SECRET = "secretkey";
 
 /* ---------------- MYSQL CONNECTION ---------------- */
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
 
 // const db = mysql.createConnection({
 //   host: "localhost",
